212da6febd39b359fa4739c59d4f9fc63c50b030
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-23 11:16:59 +0300

    add setter and getter method in the decode class

--- src/main/java/Decode.java
+++ src/main/java/Decode.java
@@ -1,3 +1,27 @@
 public class Decode {
+    private static  int key;
+    private static String sentence;
+
+
+    public Decode (int key, String sentence){
+        this.key = key;
+        this.sentence = sentence;
+    }
+
+    public static int getKey(){
+        return key;
+    }
+
+    public static String getSentence(){
+        return sentence;
+    }
+
+    public static void setKey(int key){
+        Decode.key = key;
+    }
+
+    public static void setSentence(String sentence){
+        Decode.sentence = sentence;
+    }
 
 }
