d08e239a5f94752716d1e21c8378b558dac27370
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-23 11:24:02 +0300

    test getSentence method in Decode class

--- src/test/java/DecodeTest.java
+++ src/test/java/DecodeTest.java
@@ -10,4 +10,24 @@ public class DecodeTest {
         int expectedInt = 1;
         assertEquals(expectedInt, Decode.getKey());
     }
+
+    @Test
+    public void getSentence_getCorrectSentence_String() {
+        Decode test = new Decode(1, "Hello");
+        String expectedOutput = "Hello";
+        assertEquals(expectedOutput,Decode.getSentence());
+    }
+
+
+    @Test
+    public void setKey() {
+        Encoding.setKey(20);
+        assertEquals(20, Encoding.getKey());
+    }
+
+    @Test
+    public void setSentence() {
+        Encoding.setSentence("Hello");
+        assertEquals("Hello", Encoding.getSentence());
+    }
 }
