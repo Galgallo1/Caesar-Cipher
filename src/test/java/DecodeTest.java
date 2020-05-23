cd24eec61dbdbd90c8f5372f283c1e0baa68da7d
Galgallo Tadicha <tadichgalgallo@gmail.com>
2020-05-23 11:18:02 +0300

    add decodeTest file

---
+++ src/test/java/DecodeTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DecodeTest {
+
+    @Test
+    public void getKey() {
+    }
+
+    @Test
+    public void getSentence() {
+    }
+
+    @Test
+    public void setKey() {
+    }
+
+    @Test
+    public void setSentence() {
+    }
+}
